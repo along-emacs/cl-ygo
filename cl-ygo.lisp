7abb4cfde992d1608d555587af5643849691af35
Xinlong Hu <along.emacs@gmail.com>
2019-07-12 20:22:30 +0800

    add comment for (parse-deck)

--- cl-ygo.lisp
+++ cl-ygo.lisp
@@ -78,17 +78,18 @@ where texts.id = " (write-to-string id) ";"))
   (loop for (zone list) on *card-lists* by #'cddr do
        (setf (getf *card-lists* zone) nil)))
 
-;; Format of ydk file:
-;; #created by ...
-;; #main
-;; card id 1
-;; card id 2
-;; #extra
-;; card id 3
-;; card id 4
-;; !side
-;; card id 5
 (defun parse-deck (name)
+"Format of ydk file:
+#created by ...
+#main
+card id 1
+card id 2
+#extra
+card id 3
+card id 4
+!side
+card id 5
+"
   (let ((deck-list nil)
 	(deck-path (get-dir-of *deck-dir* "/" name ".ydk"))
 	(card-id 0) (main-p nil) (extra-p nil) (side-p nil))
